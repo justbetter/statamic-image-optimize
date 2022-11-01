eb4bf988eceb49f16f96fa5df504b947365cc214
Kevin Meijer <kevin@justbetter.nl>
2022-11-01 11:34:33 +0100

    Added env option to config file

--- config/image-optimize.php
+++ config/image-optimize.php
@@ -2,8 +2,8 @@
 
 return [
     // Set the default resize width in pixels
-    'default_resize_width' => 1600,
+    'default_resize_width' => env('IMAGE_OPTIMIZE_WIDTH', 1600),
 
     // Set the default resize height in pixels
-    'default_resize_height' => 1600,
+    'default_resize_height' => env('IMAGE_OPTIMIZE_HEIGHT', 1600),
 ];
